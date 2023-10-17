50dbb61792290eb97292e8906c7b0c7c11213e52
ss749236 <ss749236@gmail.com>
2023-10-17 18:50:21 +0530

    state

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,14 +1,39 @@
-import restaurants from "../utils/mockData";
+import { useEffect, useState } from "react";
+
 import ResCard from "./ResCard";
+import Shimmer from "./Shimmer";
+
 
 
 const Body = () => {
-    return (
+
+    const [listedRestro, setListedRestro] = useState([]);
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
+    const fetchData = async () => {
+
+        const data = await fetch("https://www.swiggy.com/dapi/restaurants/list/v5?lat=12.9715987&lng=77.5945627&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING");
+        const json = await data.json();
+        console.log(json);
+        setListedRestro(json?.data?.cards[5]?.card?.card?.gridElements?.infoWithStyle?.restaurants);
+
+    }
+
+    return listedRestro.length === 0 ? <Shimmer /> : (
         <div className='body'>
-            <div className='search-container'>search</div>
+            <div className='search-container'>
+                <button onClick={() => {
+                    const filterdList = listedRestro.filter((res) => res.info.avgRating > 4.1)
+                    setListedRestro(filterdList);
+                }}>Top Rated Restaurants</button>
+            </div>
             <div className='res-container'>
+
                 <div className='res-card'>
-                    {restaurants.map((resList) => (<ResCard key={resList.info.id} resData={resList} />))}
+                    {listedRestro.map((resList) => (<ResCard key={resList.info.id} resData={resList} />))}
 
                 </div>
             </div>
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,9 @@
+import { useState } from "react";
 import { LOGO_URL } from "../utils/constant";
 
 const Header = () => {
+
+    const [btnName, setBtnName] = useState("Login");
     return (
         <div className='header'>
             <div className='logo-container'>
@@ -12,6 +15,7 @@ const Header = () => {
                     <li>About Us</li>
                     <li>Contact Us</li>
                     <li>Cart</li>
+                    <button onClick={() => { setBtnName("LogOut") }}>{btnName}</button>
                 </ul>
             </div>
         </div>
