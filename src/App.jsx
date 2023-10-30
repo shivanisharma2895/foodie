f45ad0166867861a3a405f519f8635bf6297ac9b
ss749236 <ss749236@gmail.com>
2023-10-30 13:16:00 +0530

    error-handling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,39 @@
 
 import './App.css';
-import Header from './components/Header';
-import Body from './components/Body';
+import AppLayOut from './components/AppLayOut';
+import About from './components/About';
+import Contact from './components/Contact';
+import Error from './components/Error';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+
+const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <AppLayOut />,
+      errorElement: <Error />
+    },
+    {
+      path: "/about",
+      element: <About />
+    },
+    {
+      path: "/contact",
+      element: <Contact />
+    }
+  ]
+)
+
+
 
 function App() {
   return (
-    <div className="App">
-      <Header />
-      <Body />
+    <div>
+      <RouterProvider router={router} />
     </div>
-  );
+  )
 }
-
 
 
 
---
+++ src/components/Error.js
@@ -0,0 +1,13 @@
+import { useRouteError } from "react-router-dom";
+const Error = () => {
+    const err = useRouteError();
+    console.log(err);
+    return (
+        <div>
+            <h1>Oop! something went wrong!</h1>
+            <h3>{err.statusText}</h3>
+            <h2>{err.status}</h2>
+        </div>
+    )
+}
+export default Error;
