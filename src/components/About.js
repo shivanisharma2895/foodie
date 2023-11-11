9aa3e706736e15cc56e07e04a4c10f94323146c4
ss749236 <ss749236@gmail.com>
2023-11-11 17:58:44 +0530

    class

--- src/components/About.js
+++ src/components/About.js
@@ -1,9 +1,12 @@
+import User from "./User";
+
 const About = () => {
     return (
         <div>
             <h1>
                 Hi this is Foodie.
             </h1>
+            <User />
         </div>
     )
 }
---
+++ src/components/User.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+
+
+class User extends React.Component {
+    render() {
+        return (
+            <div className="user-card">
+                <h2>Name : Shivani Sharma</h2>
+                <h3>Location: Gujrat</h3>
+                <h3>Contact Info: +918674395735</h3>
+            </div>
+        )
+    }
+}
+
+export default User;
